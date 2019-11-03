8c9d0893686ad43e6e1463480d69f0c7f5ec33f7
Ruslan <ruslanworkconsult@gmail.com>
2019-11-03 22:50:46 +0200

    done

---
+++ README.md
@@ -0,0 +1,8 @@
+# JS Band Internship() {
+
+
+
+
+
+
+# };
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// module.exports = {
+//     entry: './src/index.js',
+//     mode: 'production',
+//     output: {
+//         filename: 'build.js'
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.m?js$/,
+//                 exclude: /(node_modules|bower_components)/,
+//                 use: {
+//                     loader: 'babel-loader',
+//                     options: {
+//                         presets: ['@babel/preset-env']
+//                     }
+//                 }
+//             }
+//         ]
+//     }
+// }
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
